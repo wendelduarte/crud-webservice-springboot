55b5e859378ddf5d23c87a090a9f80c8549007e2
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-08 10:28:19 -0300

    Project created

---
+++ src/main/java/com/wendel/webservice/CrudWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wendel.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendel/webservice/CrudWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendel.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
