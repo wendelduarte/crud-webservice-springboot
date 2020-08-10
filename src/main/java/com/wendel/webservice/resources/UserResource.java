b5cf154d45d23b58590d5c97a858c8d5aeb172e8
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-10 20:22:12 -0300

    Service layer, component registration

--- src/main/java/com/wendel/webservice/resources/UserResource.java
+++ src/main/java/com/wendel/webservice/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.wendel.webservice.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wendel.webservice.entities.User;
+import com.wendel.webservice.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Wendel", "wendel@gmail.com", "40028922", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
