6c7539c094715925270f9aeb837699e6e7593534
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-10 21:34:37 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/wendel/webservice/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.wendel.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wendel.webservice.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
