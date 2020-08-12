2d0b310775f6462f8f5391e3ccbfc4e4e1cdb30b
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-12 20:35:37 -0300

    Add getSubTotal and getTotal methods

--- src/main/java/com/wendel/webservice/entities/Order.java
+++ src/main/java/com/wendel/webservice/entities/Order.java
@@ -99,6 +99,14 @@ public class Order implements Serializable{
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
+	
+	public Double getTotal() {
+		Double total = 0.0;
+		for(OrderItem orderItem : items) {
+			total += orderItem.getSubTotal();
+		}
+		return total;
+	}
 
 	@Override
 	public int hashCode() {
