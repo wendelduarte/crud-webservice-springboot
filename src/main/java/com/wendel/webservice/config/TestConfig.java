0e3aaea406a1973f04e8244195069b694205ebb9
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-12 20:26:29 -0300

    Payment, one-to-one association

--- src/main/java/com/wendel/webservice/config/TestConfig.java
+++ src/main/java/com/wendel/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.wendel.webservice.entities.Category;
 import com.wendel.webservice.entities.Order;
 import com.wendel.webservice.entities.OrderItem;
+import com.wendel.webservice.entities.Payment;
 import com.wendel.webservice.entities.Product;
 import com.wendel.webservice.entities.User;
 import com.wendel.webservice.entities.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
