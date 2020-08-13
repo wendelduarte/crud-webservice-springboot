301e5b9940ad5608f65124ba7f615f75c278b2a6
Wendel Duarte <wendel_duarte@outlook.com>
2020-08-12 22:25:55 -0300

    Exception handling - update

--- src/main/java/com/wendel/webservice/services/UserService.java
+++ src/main/java/com/wendel/webservice/services/UserService.java
@@ -3,6 +3,8 @@ package com.wendel.webservice.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User updatedUser) {
-		User user = userRepository.getOne(id);
-		updateData(user, updatedUser);
-		return userRepository.save(user);
+		try {
+			User user = userRepository.getOne(id);
+			updateData(user, updatedUser);
+			return userRepository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User user, User updatedUser) {
